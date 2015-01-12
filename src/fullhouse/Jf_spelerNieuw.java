6d33e278c49849cb80b681ab01cb1d6c23e30093
andriuxas1990 <andriuxas1990@hotmail.nl>
2015-01-12 10:00:32 +0100

    Andrius - speler_toevoegen (aanpassing)

--- src/fullhouse/Jf_spelerNieuw.java
+++ src/fullhouse/Jf_spelerNieuw.java
@@ -4,8 +4,7 @@
  */
 package fullhouse;
 
-import com.toedter.calendar.JDateChooser;
-import com.toedter.calendar.demo.DateChooserPanel;
+
 import java.awt.Color;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,17 +17,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
+
 
 /**
  *
- * @author Karen
+ * @author Andrius
  */
 public class Jf_spelerNieuw extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Jf_NewEvent
-     */
     public Jf_spelerNieuw() {
         initComponents();
         setIconImage(new ImageIcon(getClass().getResource("/resources/pokerSpade.png")).getImage());
@@ -36,29 +32,6 @@ public class Jf_spelerNieuw extends javax.swing.JFrame {
 
     }
 
-     private void getSpelersNr(){
-        try {
-            //DefaultTableModel datamodel = createVoedingModel();
-            //this.jt_Voedsel.setModel(datamodel);
-            Connection connection = FullhouseDB.getConnection();
-            
-           
-            String query = "select spelernr from Speler where spelernr = '?';";
-            PreparedStatement statement = connection.prepareStatement(query);
-             ResultSet results = statement.executeQuery();
-            
-            while(results.next()){
-                int spelernr = results.getInt("spelernr");
-                System.out.println(spelernr);
-                // Object[row][column]
-                //datamodel.addRow(rij);
-             }
-            //this.jt_Voedsel.setModel(datamodel);
-        } catch (SQLException ex) {
-            Logger.getLogger(Jf_spelerNieuw.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -277,7 +250,7 @@ public class Jf_spelerNieuw extends javax.swing.JFrame {
     private void jB_spelerToevoegenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jB_spelerToevoegenActionPerformed
 
 
-        //int nr = Integer.parseInt(jT_nieuweSpeNr.getText());
+
         String naam = jT_nieuwSpeNaam.getText();
         String adres = jT_nieuwSpeAdr.getText();
         String postcode = jT_nieuwSpePostcode.getText();
@@ -286,12 +259,12 @@ public class Jf_spelerNieuw extends javax.swing.JFrame {
         //int mob = Integer.parseInt(jT_nieuwSpeMob.getText());
         String mail = jT_nieuwSpeEmail.getText();
         Date gebortedatum = jD_nieuwSpeGeb.getDate();
-        
+
         jD_nieuwSpeGeb.setDateFormatString("dd-MM-yyyy");
         SimpleDateFormat dateFormate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
         String gebdatum = dateFormate.format(jD_nieuwSpeGeb.getDate());
-      
-       
+
+
         JOptionPane.showMessageDialog(null, "Speler " + naam + " was met succes toegevoegt in FULL♠HOUSE Database !");
         int ranking = Integer.parseInt(jT_nieuwSpeRank.getText());
         boolean instructeur = jC_nieuwSpeInstr.isSelected();
@@ -306,7 +279,7 @@ public class Jf_spelerNieuw extends javax.swing.JFrame {
         try {
             Connection connection = FullhouseDB.getConnection();
             PreparedStatement statement = connection.prepareStatement(query);
-            //statement.setInt(1, nr);
+
             statement.setString(1, naam);
             statement.setString(2, adres);
             statement.setString(3, postcode);
@@ -319,9 +292,7 @@ public class Jf_spelerNieuw extends javax.swing.JFrame {
 
 
 
-            int rows = statement.executeUpdate();
-
-            //vulVoedselTabel();
+            int rows = statement.executeUpdate();           
 
             if (rows != 1) {
                 System.out.println("FOUT!!!");
